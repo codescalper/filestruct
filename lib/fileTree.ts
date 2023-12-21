6b558383f1353b94b2dfc329abe9768b848990b5
codescalper <msfunbook@gmail.com>
2023-12-21 14:39:50 +0530

    Comenting for better understanding

--- lib/fileTree.ts
+++ lib/fileTree.ts
@@ -4,14 +4,16 @@ export interface TreeNode {
 }
 
 export const generateAsciiTree = (treeData: TreeNode): string => {
+  //This is the function that generates the tree structure from the tree data
   const lines: string[] = [];
   const traverse = (node: TreeNode, prefix = "", nextPrefix = "") => {
     lines.push(prefix + node.label);
     const isLastChild = (index: number) => index === node.nodes.length - 1;
     node.nodes.forEach((childNode, index) => {
-      const childPrefix = isLastChild(index) ? "└─ " : "├─ ";
-      const nextChildPrefix = isLastChild(index) ? "   " : "│  ";
+      const childPrefix = isLastChild(index) ? "└─ " : "├─ "; //This is the prefix that is used to draw the tree structure
+      const nextChildPrefix = isLastChild(index) ? "   " : "│  "; //This is the prefix that is used to draw the tree structure
       traverse(
+        //This is the recursive call to traverse the tree
         childNode,
         nextPrefix + childPrefix,
         nextPrefix + nextChildPrefix
